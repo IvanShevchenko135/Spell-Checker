c82ac18ad5cade912e722705c6493c99493adf2a
IvanShevchenko135 <ivanshevchenko135@gmail.com>
2020-08-08 23:03:07 +0300

    Добавил бинарный поиск

--- spell_checker_solution.cpp
+++ spell_checker_solution.cpp
@@ -1,22 +1,46 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 using namespace std;
+
+bool find(string a, vector <string> arr);
 
 int main() 
 {
 	ifstream file;
-	ifstream dictionary;
-
+	ifstream words;
+	vector <string> dictionary;
 	setlocale(LC_ALL, "en_US.UTF-8");
+
+	string word;
+
+	words.open("C:\\Vanya\\Docs\\Spell-Checker\\russian_dictionary.txt");
+	while (words >> word)
+	{
+		dictionary.push_back(word);
+	}
+	words.close();
+
 	file.open("C:\\Vanya\\Docs\\Spell-Checker\\input.txt");
-
-	string word; 
-
 	while (file >> word)
 	{
 		//TODO
 	}
-
 	return 0;
+}
+
+bool find(string key, vector <string> arr)
+{
+	int l = 0, r = arr.size() - 1, c;
+
+	while (l < r)
+	{
+		c = (l + r) / 2;
+		if (arr[c] < key) l = c + 1;
+		else r = c;
+	}
+
+	if (arr[l] == key) return true;
+	else return false;
 }
