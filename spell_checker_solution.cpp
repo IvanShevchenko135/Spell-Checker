02ec83030025d2858b0968b872420e604001c07b
IvanShevchenko135 <ivanshevchenko135@gmail.com>
2020-08-10 16:55:21 +0300

    настроил формат вывода

--- spell_checker_solution.cpp
+++ spell_checker_solution.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <vector>
 #include <regex>
@@ -16,6 +17,8 @@ int main()
 	ifstream words;
 	vector <string> dictionary;
 	regex yo_regex("¸");
+	
+	string line;
 	string word;
 
 	words.open("C:\\Vanya\\Docs\\Spell-Checker\\russian_dictionary.txt");
@@ -29,18 +32,41 @@ int main()
 	sort(dictionary.begin() + 18850, dictionary.begin() + 19130);
 
 	file.open("C:\\Vanya\\Docs\\Spell-Checker\\input.txt");
-	while (file >> word)
+
+	vector <string> mistake;
+	vector <int> row;
+	vector <int> column;
+ 
+	for (int i = 1; getline(file, line); i++)
 	{
-		transform(word.begin(), word.end(), word.begin(), tolower);
-		if ((int)word[word.size() - 1] > -1 || (int)word[word.size() - 1] < -32)
+		stringstream line_stream(line);
+		for (int j = 1; line_stream >> word; j++)
 		{
-			word.pop_back();
-		}
-		if (!find(word, dictionary))
-		{
-			cout << word << endl;
+
+			transform(word.begin(), word.end(), word.begin(), tolower);
+			int n = word.size() - 1;
+			if ((int)word[n] > -1 || (int)word[n] < -32)
+			{
+				word.pop_back();
+			}
+			if (find(word, dictionary) == false)
+			{
+				mistake.push_back(word);
+				row.push_back(i);
+				column.push_back(j);
+			}
 		}
 	}
+
+	file.close();
+
+	int n = mistake.size();
+	cout << n - 1 << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << mistake[i] << " : " << row[i] << " : " << column[i] << endl;
+	}
+
 	return 0;
 }
 
